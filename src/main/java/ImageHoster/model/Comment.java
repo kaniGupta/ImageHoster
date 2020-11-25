0cd4ed07f71e24d9a7e5d0260c1dc2b01ceff7d5
kaniGupta <kanishkgupta13@gmail.com>
2020-11-25 18:06:23 +0530

    Added Comment Entity.

---
+++ src/main/java/ImageHoster/model/Comment.java
@@ -0,0 +1,45 @@
+package ImageHoster.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "created_date")
+    private LocalDate createdDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "image_id")
+    private Image image;
+}
--- src/main/java/ImageHoster/model/Image.java
+++ src/main/java/ImageHoster/model/Image.java
@@ -1,5 +1,11 @@
 package ImageHoster.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,16 +15,17 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-//@Entity annotation specifies that the corresponding class is a JPA entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-//@Table annotation provides more options to customize the mapping.
-//Here the name of the table to be created in the database is explicitly mentioned as 'images'. Hence the table named
-// 'images' will be created in the database with all the columns mapped to all the attributes in 'Image' class
 @Table(name = "images")
 public class Image {
 
@@ -45,24 +52,15 @@ public class Image {
     @Column(name = "date")
     private Date date;
 
-    //The 'images' table is mapped to 'users' table with Many:One mapping
-    //One image can have only one user (owner) but one user can have multiple images
-    //FetchType is EAGER
     @ManyToOne(fetch = FetchType.EAGER)
-    //Below annotation indicates that the name of the column in 'images' table referring the primary key in 'users'
-    // table will be 'user_id'
     @JoinColumn(name = "user_id")
     private User user;
 
-    //The attribute contains a list of all the tags of an image
-    //Note that no column will be generated for this attribute in the database instead a new table will be created
-    //Since the mapping is Many to Many, a new table will be generated containing the two columns both referencing to
-    // the primary key of both the tables ('images', 'tags')
     @ManyToMany(fetch = FetchType.LAZY)
     private List<Tag> tags = new ArrayList<>();
 
-    public Image() {
-    }
+    @OneToMany(mappedBy = "image", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
+    private List<Comment> comments = new ArrayList<>();
 
     public Image(final int id, final String title, final String imageFile, final Date date) {
         this.id = id;
@@ -77,61 +75,5 @@ public class Image {
         this.imageFile = imageFile;
         this.description = description;
         this.date = date;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(final Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(final String title) {
-        this.title = title;
-    }
-
-    public String getImageFile() {
-        return imageFile;
-    }
-
-    public void setImageFile(final String imageFile) {
-        this.imageFile = imageFile;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(final String description) {
-        this.description = description;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(final Date date) {
-        this.date = date;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(final User user) {
-        this.user = user;
-    }
-
-    public List<Tag> getTags() {
-        return tags;
-    }
-
-    public void setTags(final List<Tag> tags) {
-        this.tags = tags;
     }
 }
